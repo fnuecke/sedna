272b6a154c6102d74504587b47e5fb01175cb1d3
Florian Nücke <florian@nuecke.de>
2020-10-19 20:44:45 +0200

    Removed binary translation stuff because I can't get it to be faster with than without anymore.

--- src/main/java/li/cil/sedna/riscv/R5CPUWatchedTrace.java
+++
@@ -1,6 +0,0 @@
-package li.cil.sedna.riscv;
-
-final class R5CPUWatchedTrace {
-    public int pc = -1;
-    public int count;
-}
--- src/main/java/li/cil/sedna/riscv/dbt/Trace.java
+++
@@ -1,8 +0,0 @@
-package li.cil.sedna.riscv.dbt;
-
-import li.cil.sedna.api.memory.MemoryAccessException;
-import li.cil.sedna.riscv.R5IllegalInstructionException;
-
-public abstract class Trace {
-    public abstract void execute() throws R5IllegalInstructionException, MemoryAccessException;
-}
